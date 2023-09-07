cbfa97f62727f757057ad731b654604fe52cfe34
vladislav-garaev <marker661@gmail.com>
2023-09-07 08:24:44 +0300

    feat: implement clear completed btn

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,26 @@ import { NewTodoInput } from './components/NewTodoInput';
 export const App = () => {
   const [todosList, setTodosList] = useState<Todo[]>([]);
   const notCompletedTodos = todosList.filter((todo) => !todo.completed);
+
+  const clearCompletedTodosHandler = () => {
+    setTodosList((prev) => prev.filter((todo) => !todo.completed));
+  };
+
   return (
     <main className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-3xl text-cyan-500 text-center">Todo</h1>
-      <NewTodoInput
+      <NewTodoInput setTodosList={setTodosList} />
+      <TodoList
+        todosList={todosList}
         setTodosList={setTodosList}
       />
-      <TodoList todosList={todosList} setTodosList={setTodosList} />
-      <p>{notCompletedTodos.length} items left</p>
+      <p className="text-gray-400">{notCompletedTodos.length} items left</p>
+      <button
+        className="text-gray-400 hover:bg-cyan-100"
+        onClick={clearCompletedTodosHandler}
+      >
+        Clear Completed
+      </button>
     </main>
   );
 };
