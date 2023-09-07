2e7fb5c9f61cf7d7aa22d84836d231f295236a73
vladislav-garaev <marker661@gmail.com>
2023-09-07 16:37:57 +0300

    feat: implement edit/save

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,32 +1,95 @@
+import { type ChangeEvent, useState } from 'react';
 import type { TodoItemProps } from '../types';
 
 export const TodoItem = ({ description, completed, setTodosList, id }: TodoItemProps) => {
+  const [editable, setEdditable] = useState<boolean>(false);
+
+  const editTodoHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+    setTodosList((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            description: e.target.value
+          };
+        }
+        return todo;
+      });
+    });
+  };
+
+  const saveTodoHandler = (): void => {
+    setTodosList((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      });
+    });
+    setEdditable(false);
+  };
+
+  const removeTodoHandler = (): void => {
+    setTodosList((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      });
+    });
+  };
+
   return (
-    <li className="w-full border-b p-2 border-cyan-500 hover:bg-slate-100">
+    <li className="relative w-full border-b p-2 border-cyan-500 hover:bg-slate-100 group">
       <label
         className={`${
           completed ? 'line-through text-gray-400' : ''
         } flex items-center gap-2 cursor-pointer overflow-auto`}
       >
-        <input
-          className="cursor-pointer"
-          type="checkbox"
-          checked={completed}
-          onChange={() => {
-            setTodosList((prev) => {
-              return prev.map((todo) => {
-                if (todo.id === id) {
-                  return {
-                    ...todo,
-                    completed: !todo.completed
-                  };
-                }
-                return todo;
-              });
-            });
-          }}
-        />
-        {description}
+        {editable
+          ? (
+          <>
+            <input
+              type="text"
+              className="w-full bg-slate-50 no-underline"
+              value={description}
+              onChange={editTodoHandler}
+            />
+            <button
+              className="absolute right-1 top-1 text-gray-400 bg-slate-50 p-1 hover:text-gray-600 group-hover:bg-slate-100 disabled:text-gray-200"
+              onClick={saveTodoHandler}
+            >
+              Save
+            </button>
+          </>
+            )
+          : (
+          <>
+            <input
+              className="cursor-pointer"
+              type="checkbox"
+              checked={completed}
+              onChange={removeTodoHandler}
+            />
+            {description}
+            <button
+              className="absolute right-1 top-1 text-gray-400 bg-slate-50 p-1 hover:text-gray-600 group-hover:bg-slate-100 disabled:text-gray-200"
+              onClick={() => {
+                setEdditable(true);
+              }}
+            >
+              Edit
+            </button>
+          </>
+            )}
       </label>
     </li>
   );
