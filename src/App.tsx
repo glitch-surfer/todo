b65db11a94ffc268704a7a57587e92bac96b0ba9
vladislav-garaev <marker661@gmail.com>
2023-09-07 08:19:35 +0300

    feat: add left items counter

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewTodoInput } from './components/NewTodoInput';
 
 export const App = () => {
   const [todosList, setTodosList] = useState<Todo[]>([]);
+  const notCompletedTodos = todosList.filter((todo) => !todo.completed);
   return (
     <main className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-3xl text-cyan-500 text-center">Todo</h1>
@@ -12,6 +13,7 @@ export const App = () => {
         setTodosList={setTodosList}
       />
       <TodoList todosList={todosList} setTodosList={setTodosList} />
+      <p>{notCompletedTodos.length} items left</p>
     </main>
   );
 };
